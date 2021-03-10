ad514f24df107f6a2f254fd5db8ba38c62038c6d
lazar.beslac <beslaclazar1@gmail.com>
2021-03-10 01:11:38 +0100

    Zavrsen projekat u .NET Core-u

---
+++ Sandbox/Lazar Beslac/backend/netCoreProba/Controllers/StudentController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using netCoreProba.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netCoreProba.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudent db;
+
+        public StudentController(IStudent db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult dajStudenta(int id)
+        {
+            Student rez = db.GetStudent(id);
+
+            return Ok(rez);
+        }
+
+        [HttpGet]
+        public IActionResult dajSveStudente()
+        {
+            IQueryable<Student> rez = db.GetStudents;
+
+            return Ok(rez);
+        }
+
+        [Route("obrisi/{id}")]
+        [HttpGet]
+        public IActionResult obrisi(int id)
+        {
+            db.Delete(id);
+
+            return Ok();
+        }
+
+        [Route("dodaj/indeks={indeks}/imePrezime={imePrezime}")]
+        [HttpGet]
+        public IActionResult dodaj(string indeks, string imePrezime)
+        {
+            db.Insert(indeks, imePrezime);
+
+            return Ok("Lepo sam pozvao");
+        }
+    }
+}
---
+++ Sandbox/Lazar Beslac/backend/netCoreProba/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netCoreProba.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string indeks { get; set; }
+        public string imePrezime { get; set; }
+    }
+}
