9e30ff2dd8d8102a200f0d9b7a4e2ffb461aa35b
Daka <darko.bjelicic99@gmail.com>
2021-03-16 18:00:52 +0100

    Klasa i funkcionalnosti za Kategorije

---
+++ App/backend/netCore/netCore/Controllers/KategorijeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using netCore.Interfaces;
+using netCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netCore.Controllers
+{
+    [Route("kategorije/")]
+    [ApiController]
+    public class KategorijeController : ControllerBase
+    {
+        private readonly IKategorije db;
+
+        public KategorijeController(IKategorije db)
+        {
+            this.db = db;
+        }
+
+        //kategorije/1
+        [HttpGet("{id}")]
+        public IActionResult dajKategoriju(int id)
+        {
+            Kategorije k = db.dajKategoriju(id);
+            return Ok(k);
+        }
+
+        //kategorije/svekategorije
+        [Route("sveKategorije")]
+        [HttpGet]
+        public IActionResult dajSveKategorije()
+        {
+            IQueryable kategorije = db.dajSveKategorije();
+            return Ok(kategorije);
+        }
+    }
+}
