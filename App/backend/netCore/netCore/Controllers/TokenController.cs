c35eb314116d6a7b5bf04b67dbecbc74b69ba268
lazar.beslac <beslaclazar1@gmail.com>
2021-04-16 01:41:42 +0200

    Napravljeni tokeni u .Netu

---
+++ App/backend/netCore/netCore/Controllers/TokenController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using netCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace netCore.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        public readonly IJWTAuthenticationManager jwtAuthenticationManager;
+
+        public TokenController(IJWTAuthenticationManager jwtAuthenticationManager)
+        {
+            this.jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+
+        // GET: api/<TokenController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "New Jersey", "New York" };
+        }
+
+        // GET api/<TokenController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token = jwtAuthenticationManager.Authenticate(userCred.Username, userCred.Password);
+            if (token == null)
+                return Unauthorized();
+            return Ok(token);
+        }
+
+    }
+}
