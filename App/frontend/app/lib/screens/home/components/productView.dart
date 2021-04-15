f4a71d0c1ee7b92d587fbc282c50ebcccff23d20
kristina.bojovic <kristinabojovic98@gmail.com>
2021-04-15 21:22:58 +0200

    prilagodjen izgled proizvoda na strani za kategorije

--- App/frontend/app/lib/screens/home/components/productView.dart
+++ App/frontend/app/lib/screens/home/components/productView.dart
@@ -12,48 +12,35 @@ class ProductView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ProizvodiModel proizvodi = Provider.of<ProizvodiModel>(context);
     return Container(
-      //backgroundColor: Color(0xFFFCFAF8),
-      //child: ListView(
-      // children: <Widget>[
-      //    SizedBox(height: 15.0),
-      //child: Container(
-      padding: EdgeInsets.only(left: 15.0),
-      margin: EdgeInsets.all(10.0),
-      //margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      width: MediaQuery.of(context).size.width - 30.0,
-      height: MediaQuery.of(context).size.height - 100.0,
-      child: Container(
-        child: GridView.count(
-          physics: NeverScrollableScrollPhysics(),
-          crossAxisCount: ResponsiveLayout.isIphone(context)
-              ? (MediaQuery.of(context).size.width / 180).round()
-              : (MediaQuery.of(context).size.width / 250).round(),
-          primary: false,
-          crossAxisSpacing: 5.0,
-          mainAxisSpacing: 10.0,
-          childAspectRatio: ResponsiveLayout.isIphone(context) ? 0.7 : 0.8,
-          children: List.generate(
-            listaProizvoda.length,
-            (index) {
-              return ProductCard(
-                  name: listaProizvoda[index].naziv,
-                  price: listaProizvoda[index].cena.toString(),
-                  imgPath: 'assets/images/cookiechoco.jpg',
-                  added: false,
-                  isFavorite: false,
-                  context: context,
-                  proizvod: listaProizvoda[index]);
-            },
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        margin: EdgeInsets.symmetric(vertical: 5.0),
+        // width: MediaQuery.of(context).size.width,
+        // height: MediaQuery.of(context).size.height - 100.0,
+        child: Stack(children: [
+          GridView.count(
+            //  physics: NeverScrollableScrollPhysics(),
+            crossAxisCount: ResponsiveLayout.isIphone(context)
+                ? 2
+                : (MediaQuery.of(context).size.width / 250).round(),
+            //primary: false,
+            // crossAxisSpacing: 5.0,
+            //  mainAxisSpacing: 10.0,
+            childAspectRatio: ResponsiveLayout.isIphone(context) ? 0.7 : 0.8,
+            children: List.generate(
+              listaProizvoda.length,
+              (index) {
+                return ProductCard(
+                    name: listaProizvoda[index].naziv,
+                    price: listaProizvoda[index].cena.toString(),
+                    imgPath: 'assets/images/cookiechoco.jpg',
+                    added: false,
+                    isFavorite: false,
+                    context: context,
+                    proizvod: listaProizvoda[index]);
+              },
+            ),
           ),
-        ),
-
-        //   SizedBox(height: 15.0)
-        //   ],
-        //   ),
-        //
-      ),
-    );
+        ]));
   }
 }
