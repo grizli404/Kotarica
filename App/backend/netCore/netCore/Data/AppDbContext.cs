4227c8d044acb4592926d92b4bd4404daa45d828
lazar.beslac <beslaclazar1@gmail.com>
2021-05-04 22:19:51 +0200

    Unapredjen chat, dodate f-je za proveru da li je korisnik na mrezi, vracanje istorije chat-a i slanja privatne poruke

--- App/backend/netCore/netCore/Data/AppDbContext.cs
+++ App/backend/netCore/netCore/Data/AppDbContext.cs
@@ -3,6 +3,7 @@ using netCore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+//using System.Reflection;
 using System.Threading.Tasks;
 
 namespace netCore.Data
@@ -11,5 +12,16 @@ namespace netCore.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+
+        public DbSet<Message> Message { get; set; }
+
+        /*
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        */
     }
 }
