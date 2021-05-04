027452fbf3959c15051f5b5f50dc2f5a24c75b72
Andrija <131-2018@pmf.kg.ac.rs>
2021-05-04 22:14:43 +0200

    omiljeni proizvodi fiksiranja lista

--- App/frontend/app/lib/screens/favorites/components/favorites_body.dart
+++ App/frontend/app/lib/screens/favorites/components/favorites_body.dart
@@ -7,27 +7,45 @@ import '../../../model/listaZeljaModel.dart';
 import '../../../model/proizvodiModel.dart';
 
 class FavoritesBody extends StatelessWidget {
-  const FavoritesBody({
-    Key key,
-  }) : super(key: key);
+  //ListaZeljaModel lzModel;
+  ProizvodiModel pModel;
+  List<Proizvod> listaOmiljenihProizvoda = [];
+  List<int> listaIndeksaOmiljenihProizvoda = [];
 
-  @override
-  Widget build(BuildContext context) {
-    ListaZeljaModel lzModel = new ListaZeljaModel();
-    ProizvodiModel pModel = new ProizvodiModel();
-    lzModel.dajLajkove(korisnikInfo.id);
-    List<Proizvod> listaOmiljenihProizvoda = [];
-    List<int> listaIndeksaOmiljenihProizvoda = lzModel.listaLajkovanihProizvoda;
+  FavoritesBody({
+    Key key,
+  }) {
+    initObjects();
+    init();
+  }
+
+  void init() async {
+    print("here1");
+    //await this.lzModel.dajLajkove(korisnikInfo.id);
+    listaZeljaModelMain.dajLajkove(korisnikInfo.id);
+    print("here2");
+    this.listaIndeksaOmiljenihProizvoda =
+        listaZeljaModelMain.listaLajkovanihProizvoda;
 
     for (int i = 0; i < listaIndeksaOmiljenihProizvoda.length; i++) {
       listaOmiljenihProizvoda
           .add(pModel.dajProizvodZaId(listaIndeksaOmiljenihProizvoda[i]));
       print(pModel.dajProizvodZaId(listaIndeksaOmiljenihProizvoda[i]).naziv);
     }
+    print("proso omiljene");
+  }
+
+  void initObjects() {
+    //this.lzModel = new ListaZeljaModel();
+    this.pModel = new ProizvodiModel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         child: Wrap(
-          children: generateChildren(listaOmiljenihProizvoda),
+          children: generateChildren(testProizvodi),
         ));
   }
 
@@ -48,47 +66,47 @@ class FavoritesBody extends StatelessWidget {
   }
 }
 
-// List<Proizvod> testProizvodi = [
-//   new Proizvod(
-//     cena: 100,
-//     id: 100,
-//     idKorisnika: 1,
-//     kolicina: 100,
-//     naziv: "Jabuke",
-//   ),
-//   new Proizvod(
-//     cena: 100,
-//     id: 100,
-//     idKorisnika: 1,
-//     kolicina: 100,
-//     naziv: "Kruske",
-//   ),
-//   new Proizvod(
-//     cena: 100,
-//     id: 100,
-//     idKorisnika: 1,
-//     kolicina: 100,
-//     naziv: "Jabuke",
-//   ),
-//   new Proizvod(
-//     cena: 100,
-//     id: 100,
-//     idKorisnika: 1,
-//     kolicina: 100,
-//     naziv: "Kruske",
-//   ),
-//   new Proizvod(
-//     cena: 100,
-//     id: 100,
-//     idKorisnika: 1,
-//     kolicina: 100,
-//     naziv: "Jabuke",
-//   ),
-//   new Proizvod(
-//     cena: 100,
-//     id: 100,
-//     idKorisnika: 1,
-//     kolicina: 100,
-//     naziv: "Kruske",
-//   ),
-// ];
+List<Proizvod> testProizvodi = [
+  new Proizvod(
+    cena: 100,
+    id: 100,
+    idKorisnika: 1,
+    kolicina: 100,
+    naziv: "Jabuke",
+  ),
+  new Proizvod(
+    cena: 100,
+    id: 100,
+    idKorisnika: 1,
+    kolicina: 100,
+    naziv: "Kruske",
+  ),
+  new Proizvod(
+    cena: 100,
+    id: 100,
+    idKorisnika: 1,
+    kolicina: 100,
+    naziv: "Jabuke",
+  ),
+  new Proizvod(
+    cena: 100,
+    id: 100,
+    idKorisnika: 1,
+    kolicina: 100,
+    naziv: "Kruske",
+  ),
+  new Proizvod(
+    cena: 100,
+    id: 100,
+    idKorisnika: 1,
+    kolicina: 100,
+    naziv: "Jabuke",
+  ),
+  new Proizvod(
+    cena: 100,
+    id: 100,
+    idKorisnika: 1,
+    kolicina: 100,
+    naziv: "Kruske",
+  ),
+];
