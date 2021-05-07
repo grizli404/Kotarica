a6b563359c0176cbf0fa9f30b2d8ed4186aad3d4
Andrija <131-2018@pmf.kg.ac.rs>
2021-05-07 20:08:53 +0200

    dodat tudj profil, nisu izlistani njegovi proizvodi jos (bug mora dva put da se klikne na korisnika)

--- App/frontend/app/lib/screens/profile/profile_screen.dart
+++ App/frontend/app/lib/screens/profile/profile_screen.dart
@@ -1,6 +1,9 @@
+import 'package:app/components/responsive_layout.dart';
 import 'package:app/model/korisniciModel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../constants.dart';
 
 class ProfileScreen extends StatelessWidget {
   final Korisnik korisnik;
@@ -10,16 +13,155 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Profilna Strana"),
+        title: Text(korisnik.ime + " " + korisnik.prezime),
       ),
-      body: Container(
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
           child: Column(
-        children: [
-          Text("ime: " + korisnik.ime),
-          Text("prezime: " + korisnik.prezime),
-          Text("adresa: " + korisnik.adresa)
-        ],
-      )),
+            children: [
+              Wrap(
+                crossAxisAlignment: WrapCrossAlignment.center,
+                direction: Axis.horizontal,
+                children: [
+                  Container(
+                    width: 400,
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.all(20),
+                          width: 110,
+                          height: 110,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: kPrimaryColor,
+                          ),
+                          child: Container(
+                            margin: EdgeInsets.all(10),
+                            width: 100,
+                            height: 100,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: AssetImage(
+                                    "assets/images/defaultProfilePhoto.png"),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  "Ime  ",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: kPrimaryColor,
+                                  ),
+                                ),
+                                Text(
+                                  korisnik.ime,
+                                  style: TextStyle(fontSize: 14),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "Prezime  ",
+                                  style: TextStyle(
+                                      fontSize: 16, color: kPrimaryColor),
+                                ),
+                                Text(
+                                  korisnik.prezime,
+                                  style: TextStyle(fontSize: 14),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "Adresa  ",
+                                  style: TextStyle(
+                                      fontSize: 16, color: kPrimaryColor),
+                                ),
+                                Text(
+                                  korisnik.adresa,
+                                  style: TextStyle(fontSize: 14),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "Broj telefona  ",
+                                  style: TextStyle(
+                                      fontSize: 16, color: kPrimaryColor),
+                                ),
+                                Text(
+                                  korisnik.brojTelefona,
+                                  style: TextStyle(fontSize: 14),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "Email  ",
+                                  style: TextStyle(
+                                      fontSize: 16, color: kPrimaryColor),
+                                ),
+                                Text(
+                                  korisnik.mail,
+                                  style: TextStyle(fontSize: 14),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: ResponsiveLayout.isIphone(context)
+                        ? MediaQuery.of(context).size.width * 0.4
+                        : MediaQuery.of(context).size.width * 0.3,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25.0),
+                        color: Theme.of(context).primaryColor),
+                    child: Center(
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.chat_outlined, color: Colors.white),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              'Pošalji poruku',
+                              style: TextStyle(
+                                  fontFamily: 'Varela',
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ]),
+                    ),
+                  ),
+                ],
+              ),
+              Divider(
+                color: kPrimaryColor,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
